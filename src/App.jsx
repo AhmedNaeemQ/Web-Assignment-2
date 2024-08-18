cab807aa8b2b4d46b69dabd60404e1296c509fea
Ahmed Naeem <ahmednaeem6603@gmail.com>
2024-08-18 15:53:05 +0500

    Added Navigations to Navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,61 @@ import { Skills } from "./components/Skills/Skills";
 import { Work } from "./components/Work/Work";
 import { Contact } from "./components/Contact/Contact";
 import { Achivements } from "./components/Achievements/Achievements";
+import { useRef } from "react";
 
 function App() {
+  const homeRef = useRef(null);
+  // const aboutRef = useRef(null);
+  const skillsRef = useRef(null);
+  // const projectsRef = useRef(null);
+  const workRef = useRef(null);
+  // const achievementsRef = useRef(null);
+  const resumeRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <div className="px-12 ">
-      <Navbar />
-      <Home />
-      <Aboutme />
-      <Skills />
-      <Projects />
-      <Work />
-      <Achivements />
-      <Resume />
-      <Contact />
+    <div className="px-12">
+      <Navbar
+        scrollToSection={scrollToSection}
+        refs={{
+          homeRef,
+          // aboutRef,
+          skillsRef,
+          // projectsRef,
+          workRef,
+          // achievementsRef,
+          resumeRef,
+          contactRef,
+        }}
+      />
+      <div ref={homeRef}>
+        <Home />
+      </div>
+      {/* <div ref={aboutRef}> */}
+        <Aboutme />
+      {/* </div> */}
+      <div ref={skillsRef}>
+        <Skills />
+      </div>
+      {/* <div ref={projectsRef}> */}
+        <Projects />
+      {/* </div> */}
+      <div ref={workRef}>
+        <Work />
+      </div>
+      {/* <div ref={achievementsRef}> */}
+        <Achivements />
+      {/* </div> */}
+      <div ref={resumeRef}>
+        <Resume />
+      </div>
+      <div ref={contactRef}>
+        <Contact />
+      </div>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,19 @@
 import { GradientButton } from "../GradientButton/Button";
 
-export const Navbar = () => {
-  const navItems = ["Home", "Projects", "Blog", "Contact"];
+export const Navbar = ({ scrollToSection, refs }) => {
+  const navItems = ["Home", "Skills", "Work", "Resume"];
+
+  const navigate = (section) => {
+    if (section === "Home") {
+      scrollToSection(refs.homeRef);
+    } else if (section === "Skills") {
+      scrollToSection(refs.skillsRef);
+    } else if (section === "Work") {
+      scrollToSection(refs.workRef);
+    } else if (section === "Resume") {
+      scrollToSection(refs.resumeRef);
+    }
+  };
 
   return (
     <div className="fixed flex justify-between my-4 px-4 h-16 w-[calc(100%-6rem)] rounded-full border items-center backdrop-blur z-50">
@@ -11,14 +23,19 @@ export const Navbar = () => {
 
       <ul className="font-bigjohn-regular flex gap-20 h-full">
         {navItems.map((item) => (
-          <button className="relative group h- overflow-hidden">
+          <button
+            onClick={() => navigate(item)}
+            className="relative group h- overflow-hidden"
+          >
             <span className="bg-gradient-to-r from-violet-500 to-sky-500 w-full h-1 absolute z-10 bottom-3 transition ease-in duration-200 -translate-x-[110%] group-hover:translate-x-0"></span>
             {item}
           </button>
         ))}
       </ul>
 
-      <GradientButton />
+      <div onClick={() => scrollToSection(refs.contactRef)}>
+        <GradientButton />
+      </div>
     </div>
   );
 };
